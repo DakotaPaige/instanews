2d32b3d15dbdaff9d446a1bc3165fcc0a5a96b37
Dakota M <dakota.paige@hotmail.com>
2018-01-24 16:12:02 -0800

    Added gulp files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,55 @@
-var gulp = require('gulp');
+var gulp = require('gulp'); //Load gulp first
 
-gulp.task('default', defaultTask);
+//Load the uglify and rename packages
+var uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    browserSync = require('browser-sync').create(),
+    eslint = require('gulp-eslint');
 
-function defaultTask(done) {
-  // place code for your default task here
-  done();
-}
+var reload = browserSync.reload();
+
+gulp.task('lint', function() {
+    return gulp.src(['./JS/*.js'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError())
+})
+
+//script task to minify, rename and put in build folder
+gulp.task('script', gulp.series('lint', function () {
+  return gulp.src('./JS/*.js') //What files we want gulp to consume
+    .pipe(uglify()) // Calls the uglify function on those files
+    .pipe(rename({extname: '.min.js'})) //Rename the uglified file
+    .pipe(gulp.dest('./build/js')); //Where are we putting the result
+}));
+
+//task to watch when a js file is edited, then run script when it is
+gulp.task('watch', function() {
+    gulp.watch('js/*.js', gulp.series('script'));
+ });
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+
+    gulp.watch('./build/js/*.js').on('change', browserSync.reload );
+})
+
+
+// gulp.task('css-watch', function(done) {
+//     browserSync.init({
+//         server: {
+//             baseDir: "./"
+//         }
+//     });
+//     gulp.watch('*.css', function() {
+//         browserSync.reload();
+//         done();
+//     })
+// });
+
+//default function that can reference multiple named tasks
+gulp.task('default', gulp.parallel('watch', 'browser-sync'));
