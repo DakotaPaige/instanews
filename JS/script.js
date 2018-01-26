61bd65a81f71034036f43b97ff53c006ca421526
Dakota M <dakota.paige@hotmail.com>
2018-01-25 16:27:21 -0800

    Changed CSS to SCSS andstarted on the ajax

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,152 @@
 $(function() {
   console.log('this is working');
-  // Built by LucyBot. www.lucybot.com
-  var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
-  url += '?' + $.param({
-    'api-key': "e92ad80f358b449e91cfa8a9d72efaf3"
-  });
-  $.ajax({
-    url: url,
-    method: 'GET',
-  }).done(function(result) {
-    console.log(result);
-  }).fail(function(err) {
-    throw err;
-  });
+
+  $ ('#news_type').change(function() {
+    var selectedSection = $('#news_type option:selected').val();
+    alert(selectedSection);
+
+    //HOME SECTION
+    if (selectedSection === 'home') {
+      $('ul').children().remove();
+      // Built by LucyBot. www.lucybot.com
+      var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+      url += '?' + $.param({
+        'api-key': "e92ad80f358b449e91cfa8a9d72efaf3"
+      });
+      $.ajax({
+        url: url,
+        method: 'GET',
+      }).done(function(data) {
+        var results = data.results;
+        console.log(results);
+        i = 0;
+
+        //iterates through each key value, grabs the description, prints it to the console and stops when it hits 12
+        $.each(results, function(key,value) {
+          var description = value.abstract;
+          console.log(description);
+          //iterates through each key value only in multimedia and grabs only the SuperJumbo image and puts it in imageURL
+          $.each(value.multimedia, function(key,value) {
+            if (value.format === "superJumbo") {
+              var imageURL = value.url;
+              var listItem = '';
+              listItem+='<li>';
+              listItem+='<img src = "'
+              listItem+=imageURL;
+              listItem+='">';
+              listItem+='<p>';
+              listItem+=description;
+              listItem+='</p></li>'
+              $('ul').append(listItem);
+            }
+            else {
+              return
+            }
+            
+
+          console.log(listItem);
+          })
+          return (key != 11)
+        })
+
+
+      }).fail(function(err) {
+        throw err;
+      });
+    }
+
+    //ARTS SECTION
+    else if (selectedSection === 'arts') {
+      $('ul').children().remove();
+      var url = "https://api.nytimes.com/svc/topstories/v2/arts.json";
+      url += '?' + $.param({
+        'api-key': "e92ad80f358b449e91cfa8a9d72efaf3"
+      });
+      $.ajax({
+        url: url,
+        method: 'GET',
+      }).done(function(data) {
+        var results = data.results;
+        console.log(results);
+      }).fail(function(err) {
+        throw err;
+      });
+    }
+
+    //BUSINESS SECTION
+    else if (selectedSection === 'business') {
+      $('ul').children().remove();
+      var url = "https://api.nytimes.com/svc/topstories/v2/business.json";
+      url += '?' + $.param({
+        'api-key': "e92ad80f358b449e91cfa8a9d72efaf3"
+      });
+      $.ajax({
+        url: url,
+        method: 'GET',
+      }).done(function(data) {
+        var results = data.results;
+        console.log(results);
+      }).fail(function(err) {
+        throw err;
+      });
+    }
+
+    //SPORTS SECTION
+    else if (selectedSection === 'sports') {
+      $('ul').children().remove();
+      var url = "https://api.nytimes.com/svc/topstories/v2/sports.json";
+      url += '?' + $.param({
+        'api-key': "e92ad80f358b449e91cfa8a9d72efaf3"
+      });
+      $.ajax({
+        url: url,
+        method: 'GET',
+      }).done(function(data) {
+        var results = data.results;
+        console.log(results);
+      }).fail(function(err) {
+        throw err;
+      });
+    }
+
+    //TECHNOLOGY SECTION
+    else if (selectedSection === 'technology') {
+      $('ul').children().remove();
+      var url = "https://api.nytimes.com/svc/topstories/v2/technology.json";
+      url += '?' + $.param({
+        'api-key': "e92ad80f358b449e91cfa8a9d72efaf3"
+      });
+      $.ajax({
+        url: url,
+        method: 'GET',
+      }).done(function(data) {
+        var results = data.results;
+        console.log(results);
+      }).fail(function(err) {
+        throw err;
+      });
+    }
+
+    //FASHION SECTION
+    else if (selectedSection === 'fashion') {
+      $('ul').children().remove();
+      var url = "https://api.nytimes.com/svc/topstories/v2/fashion.json";
+      url += '?' + $.param({
+        'api-key': "e92ad80f358b449e91cfa8a9d72efaf3"
+      });
+      $.ajax({
+        url: url,
+        method: 'GET',
+      }).done(function(data) {
+        var results = data.results;
+        console.log(results);
+      }).fail(function(err) {
+        throw err;
+      });
+    }
+  })
+
+ 
+
+  
 })
